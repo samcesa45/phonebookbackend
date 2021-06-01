833b9638d03bbcb5d1049e0a330561f83e41db1b
samcesa45 <samcesa45@gmail.com>
2021-06-01 03:16:49 -0700

    modified build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('person', (request) => {
 	if (request.method === 'POST') {
